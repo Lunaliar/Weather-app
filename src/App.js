4e9f9a2236b570645978666b1e5f2310d9f67830
Sav <98275089+Lunaliar@users.noreply.github.com>
2022-10-22 11:34:15 -0400

    push 02

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { computeHeadingLevel } from "@testing-library/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+	const [geo, setName] = useState({
+		city:"Charlottesville",
+		state:'va'
+	});
+	const [data, setData] = useState({});
+	const cityUrl =
+		`http://api.openweathermap.org/geo/1.0/direct?q=${geo.city.toLowerCase()},${geo.state.toLowerCase}&limit=1&appid=${API key}`
+	const weatherUrl = "";
+
+	useEffect(() => {
+		console.log("using that effect!");
+		axios.get;
+	}, []);
 	return (
 		<div className="App">
-			<h1>Greenlight!</h1>
+			<h1></h1>
 		</div>
 	);
 }
