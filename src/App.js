81c0524fe2fb4c334bf7f9d14856bf0b99dde94a
Sav <98275089+Lunaliar@users.noreply.github.com>
2022-10-24 20:54:10 -0400

    push 12

--- src/App.js
+++ src/App.js
@@ -4,14 +4,54 @@ import "./style.scss";
 const apiKey = process.env.REACT_APP_KEY;
 
 function App() {
+	//!State
 	const [data, setData] = useState({});
 	const [input, setInput] = useState("");
-	const [cityData, setCityData] = useState({});
 	const [geo, setGeo] = useState({
 		name: "",
 		units: true,
 	});
 
+	const componentParams = [
+		{
+			title: "Hi-Temp",
+			icon: "temperature-arrow-up",
+			data: data.main?.temp_max,
+			unit: geo.units ? " °F" : " °C",
+		},
+		{
+			title: "Low-Temp",
+			icon: "temperature-arrow-down",
+			data: data.main?.temp_min,
+			unit: geo.units ? " °F" : " °C",
+		},
+		{
+			title: "Hi-Temp",
+			icon: "temperature-arrow-up",
+			data: data.main?.temp_max,
+			unit: geo.units ? " °F" : " °C",
+		},
+		{
+			title: "Low-Temp",
+			icon: "temperature-arrow-down",
+			data: data.main?.temp_min,
+			unit: geo.units ? " °F" : " °C",
+		},
+		{
+			title: "Humidity",
+			icon: "droplet",
+			data: data.main?.humidity,
+			unit: " %",
+		},
+		{
+			title: "Wind",
+			icon: "wind",
+			data: data.wind?.speed,
+			unit: geo.units ? " mph" : " km/h",
+		},
+	];
+
+	//? useEffect & Mount
 	const mounted = useRef(false);
 
 	useEffect(() => {
@@ -20,7 +60,6 @@ function App() {
 			axios
 				.get(cityUrl)
 				.then((res) => {
-					setCityData(res.data);
 					return axios.get(
 						`https://api.openweathermap.org/data/2.5/weather?lat=${
 							res.data[0].lat
@@ -43,6 +82,7 @@ function App() {
 		}
 	}, [geo]);
 
+	//! Functions
 	const handleKey = (e) => {
 		if (e.key === "Enter") {
 			setGeo({ ...geo, name: input });
@@ -66,109 +106,87 @@ function App() {
 	const handleUnits = () => {
 		setGeo({ ...geo, units: !geo.units });
 	};
-
+	//? Components
 	const search = () => {
 		return (
 			<div className="search">
 				<input
 					required
+					value={input}
 					type="text"
 					onKeyDown={handleKey}
-					value={input}
 					onChange={(e) => setInput(e.target.value)}
 				/>
 				<span>Pick a city...</span>
 			</div>
 		);
 	};
+	const minorComponent = (title, icon, data, unit) => {
+		return (
+			<div title={`${title}`}>
+				<i className={`fa-solid fa-${icon}`} />
+				<span>{data}</span>
+				{unit}
+			</div>
+		);
+	};
+	const minorPairings = (clssnme, first, second) => {
+		return (
+			<div className={clssnme}>
+				{minorComponent(first.title, first.icon, first.data, first.unit)}
+				{minorComponent(second.title, second.icon, second.data, second.unit)}
+			</div>
+		);
+	};
+	const unitButton = () => {
+		return (
+			<button
+				className="change-units"
+				title="Change Units"
+				onClick={handleUnits}
+			>
+				{geo.units ? "Metric" : "Imperial"} units
+			</button>
+		);
+	};
+	const mainInfo = () => {
+		return (
+			<>
+				<span className="city-name">{data?.name}</span>
+				<span className="current-temp">{`${data?.main.temp} ${
+					geo.units ? "°F" : "°C"
+				}`}</span>
+			</>
+		);
+	};
+	const weatherIcon = () => {
+		return (
+			<div className="type-icon">
+				<span className="weather-main">{data.weather[0]?.main}</span>
+				<img
+					title={data.weather[0]?.main}
+					src={`http://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}
+					alt="Weather Icon"
+				/>
+			</div>
+		);
+	};
+
 	return (
 		<div className="App">
 			{data.main ? (
 				<div className="main ">
 					{search()}
-
 					<div className="major-conditions">
-						<div className="button">
-							<button
-								className="change-units"
-								title="Change Units"
-								onClick={handleUnits}
-							>
-								{geo.units ? "Metric" : "Imperial"} units
-							</button>
-						</div>
-						<div className="info">
-							<span className="city-name">{data?.name}</span>
-							<span className="state-name">{cityData[0]?.state}</span>
-							<span className="current-temp">{`${data?.main.temp} ${
-								geo.units ? "°F" : "°C"
-							}`}</span>
-						</div>
+						<div className="button">{unitButton()}</div>
+						<div className="info">{mainInfo()}</div>
 					</div>
-
-					<div className="type-icon">
-						<span className="weather-main">{data.weather[0]?.main}</span>
-						<img
-							title={data.weather[0]?.main}
-							src={`http://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}
-							alt="Weather Icon"
-						/>
-					</div>
+					{weatherIcon()}
 
 					<div className="minor-conditions">
-						<div className="left">
-							<div
-								className="sunrise"
-								title="Sunrise"
-							>
-								<i className="fa-solid fa-sun" />
-								<span>{getTime(data.sys?.sunrise)}</span>
-							</div>
-							<div
-								className="sunset"
-								title="Sunset"
-							>
-								<i className="fa-solid fa-moon" />
-								<span>{getTime(data.sys?.sunset)}</span>
-							</div>
-						</div>
-
-						<div className="center">
-							<div
-								className="hi-temp"
-								title="Hi-Temp"
-							>
-								<i className="fa-solid fa-temperature-arrow-up" />
-								<span>{data.main?.temp_max}</span>
-								{geo.units ? " °F" : " °C"}
-							</div>
-							<div
-								className="low-temp"
-								title="Low-Temp"
-							>
-								<i className="fa-solid fa-temperature-arrow-down" />
-								<span>{data.main?.temp_min}</span>
-								{geo.units ? " °F" : " °C"}
-							</div>
-						</div>
-						<div className="right">
-							<div
-								className="humiditity"
-								title="Humidity"
-							>
-								<i className="fa-solid fa-droplet" />
-								<span>{data.main?.humidity}</span>
-								{" %"}
-							</div>
-							<div
-								className="wind"
-								title="Wind"
-							>
-								<i className="fa-solid fa-wind" />
-								<span>{data.wind?.speed}</span>
-								{geo.units ? " mph" : " km/h"}
-							</div>
-						</div>
+						{minorPairings("left", componentParams[0], componentParams[1])}
+						{minorPairings("center", componentParams[2], componentParams[3])}
+						{minorPairings("right", componentParams[4], componentParams[5])}
 					</div>
 				</div>
 			) : (
